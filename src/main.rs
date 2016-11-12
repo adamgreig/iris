d6309803cef7ec098ae2b5543bb901864458bd36
Adam Greig <adam@adamgreig.com>
2016-11-12 19:05:34 +0000

    Initial commit. Adds basic Flite TTS support.

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod tts;
+
+fn main() {
+    tts::init();
+    tts::say("This is a test of the emergency broadcast system");
+}
---
+++ src/tts.rs
@@ -0,0 +1,38 @@
+use std::os::raw::c_char;
+use std::ffi::CString;
+
+enum CSTVoice {}
+
+#[link(name="flite")]
+extern {
+    fn flite_init();
+    fn flite_text_to_speech(text: *const c_char, voice: *mut CSTVoice,
+                            outtype: *const c_char) -> f32;
+}
+
+#[link(name="flite_cmu_us_slt")]
+extern {
+    fn register_cmu_us_slt(voxdir: *const c_char) -> *mut CSTVoice;
+}
+
+#[link(name="flite_usenglish")]
+extern {}
+
+#[link(name="flite_cmulex")]
+extern {}
+
+pub fn init() {
+    unsafe {
+        flite_init();
+    }
+}
+
+pub fn say(text: &str) {
+    let t = CString::new(text).unwrap();
+    let voxdir = CString::new("").unwrap();
+    let play = CString::new("play").unwrap();
+    unsafe {
+        let v: *mut CSTVoice = register_cmu_us_slt(voxdir.as_ptr());
+        flite_text_to_speech(t.as_ptr(), v, play.as_ptr());
+    }
+}
